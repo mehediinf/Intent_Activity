e333ce2eb9012e3e2ed9c57ab9f4d1f5b0a0879f
mehedi-hasan <mehediinform@gmail.com>
2024-01-11 00:22:34 +0600

    Second Commit Intent_Activity

--- app/src/main/java/com/example/intent_activity/First_Intent_Activity.java
+++ app/src/main/java/com/example/intent_activity/First_Intent_Activity.java
@@ -2,13 +2,29 @@ package com.example.intent_activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class First_Intent_Activity extends AppCompatActivity {
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.first_intent_activity_main);
+
+        button = findViewById(R.id.button_Id);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(First_Intent_Activity.this,Second_Intent_Activity.class);
+                startActivity(intent);
+            }
+        });
+
+
     }
 }
